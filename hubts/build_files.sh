8c52adc256fd2b5e304dfa9005ee3eb2abb08121
tonystefan <tonystefanlog@hotmail.com>
2025-04-22 10:12:28 -0300

    teste2

---
+++ hubts/build_files.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Migrando banco de dados..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Coletando arquivos estáticos..."
+python3 manage.py collectstatic --noinput
