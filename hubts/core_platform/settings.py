f5d40fd20966a7e08c8e02bd8f37bd0388f22c8a
tonystefan <tonystefanlog@hotmail.com>
2025-04-21 21:20:14 -0300

    to deploy

--- hubts/core_platform/settings.py
+++ hubts/core_platform/settings.py
@@ -5,7 +5,7 @@ Django settings for core_platform project.
 import os
 from pathlib import Path
 import dj_database_url
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,21 +82,20 @@ WSGI_APPLICATION = 'core_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'OPTIONS': {
+            'options': '-c search_path=producao,public'
+        },
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'DISABLE_SERVER_SIDE_CURSORS': True, 
     }
 }
 
-# Use PostgreSQL in production
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
